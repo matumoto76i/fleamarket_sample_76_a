e6835e54fe11a81054eab7bd4297deaf1b8a6325
matumoto76i <34masamasa52@gmail.com>
2020-08-05 19:00:29 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,6 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
-  config.fog_directory  = 'chatspace76im'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace76im'
+    config.fog_directory  = 'chatspace76im'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace76im'
+  end
 end
