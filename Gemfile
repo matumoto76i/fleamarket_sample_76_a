bad5cbc45c892dfabfe4a2fb1089550d8969ffee
ik425524 <yipingxiaoxi286@gmail.com>
2020-08-09 16:36:24 +0900

    ホーム画面の実装
    why
    ホーム画面の実装
    what
    完了しました

--- Gemfile
+++ Gemfile
@@ -69,6 +69,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem 'font-awesome-sass'
+gem 'jquery-rails'
 
 group :production do
   gem 'unicorn'
---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,43 @@
+$(function () {
+  // マウスを当てた時に画像が透過される処理
+  $(".content__btn__img").hover(
+    function () {
+      $(this).fadeTo(100, 0.5);
+    },
+    function () {
+      $(this).fadeTo(100, 1);
+    }
+  );
+  $(".dl__btn__link").hover(
+    function () {
+      $(this).fadeTo(100, 0.5);
+    },
+    function () {
+      $(this).fadeTo(100, 1);
+    }
+  );
+  $(".container__box__list").hover(
+    function () {
+      $(this).fadeTo(100, 0.5);
+    },
+    function () {
+      $(this).fadeTo(100, 1);
+    }
+  );
+  $(".link__icon__btn").hover(
+    function () {
+      $(this).fadeTo(100, 0.5);
+    },
+    function () {
+      $(this).fadeTo(100, 1);
+    }
+  );
+  $(".purchase__btn").hover(
+    function () {
+      $(this).fadeTo(100, 0.9);
+    },
+    function () {
+      $(this).fadeTo(100, 1);
+    }
+  );
+});
