8390fa07e6a14d3cd4308f08ea675eec1f562306
ik425524 <yipingxiaoxi286@gmail.com>
2020-08-21 23:31:02 +0900

    マイページの実装
    what
    ユーザーが確認する為のマイページの実装
    
    why
    マークアップの技術向上

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'posts#index'
   get 'confirm', to: 'posts#confirm'
+  resources :users, only: [:index,:show]
   resources :products, only: [:index, :new, :create]
 end
