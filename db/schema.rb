d4c83aad17bfe9a5e9e347b7a2b2df54bdf9c13b
ue-bose <a.r.1969ueda@icloud.com>
2020-08-28 22:56:32 +0900

    ユーザーモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_125533) do
+ActiveRecord::Schema.define(version: 2020_08_28_135342) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,7 +44,18 @@ ActiveRecord::Schema.define(version: 2020_08_27_125533) do
     t.integer "period_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "images", "products"
+  add_foreign_key "products", "users"
 end
