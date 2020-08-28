d7a9478406f365663f451b559912117ed1ca3f81
matumoto76i <34masamasa52@gmail.com>
2020-08-28 18:37:10 +0900

    ウィザード形式での実装１

---
+++ app/models/profile.rb
@@ -0,0 +1,4 @@
+class Profile < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :first_name, :last_name, presence: true
+end
---
+++ db/migrate/20200826132149_create_profiles.rb
@@ -0,0 +1,14 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :first_name,       null: false
+      t.string :last_name,        null: false
+      t.string :first_kana,       null: false
+      t.string :last_kana,        null: false
+      t.string :phone
+      t.timestamps
+      t.references :user
+    end
+    add_foreign_key :profiles, :users
+  end
+end
