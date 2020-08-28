454c09db44739bfdce86f8c9e95ba38e3b2e8858
ue-bose <a.r.1969ueda@icloud.com>
2020-08-28 22:11:37 +0900

    配送料の負担

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   has_many :images
   belongs_to :category
   accepts_nested_attributes_for :images, allow_destroy: true
-  validates :region,:state,:period, presence: true
+  validates :region,:state,:period,:cost, presence: true
 end
