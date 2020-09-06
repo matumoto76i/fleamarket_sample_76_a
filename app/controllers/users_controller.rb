57ef2f9164e5c54e72e97eb2e3af84f4605481a8
matumoto76i <34masamasa52@gmail.com>
2020-09-06 14:04:43 +0900

    使用していない余分な記述を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,10 @@
 class UsersController < ApplicationController
   def index
-    # @users = User.all
   end
 
   def new
-    # @user = User.new
   end
 
   def create
-    # @user = User.new(user_params)
-    # @user.save
-    # redirect_to users_path(@user)
   end
 end
