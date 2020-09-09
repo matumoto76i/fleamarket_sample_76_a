7cdc802ea134001374ec542ee90e33d0aead2507
ue-bose <a.r.1969ueda@icloud.com>
2020-09-10 00:29:36 +0900

    モデルの編集

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,14 @@
 class Product < ApplicationRecord
-  has_many :images
+  has_many :images, dependent: :destroy
   belongs_to :category
   accepts_nested_attributes_for :images, allow_destroy: true
-  validates :region,:state,:period,:cost, presence: true
+  validates :name, presence: true, length: {maximum: 40}
+  validates :description,:category,:region,:state,:period,:cost, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :region
+  belongs_to_active_hash :state
+  belongs_to_active_hash :period
+  belongs_to_active_hash :cost
+  belongs_to_active_hash :size
 end
