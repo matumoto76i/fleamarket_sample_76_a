87f3dd19a109fb29cd5427551a99220751fc89b7
ue-bose <a.r.1969ueda@icloud.com>
2020-09-12 12:01:58 +0900

    新規登録の編集

--- app/assets/javascripts/product_new.js
+++ app/assets/javascripts/product_new.js
@@ -1,152 +1,90 @@
 $(document).on('turbolinks:load', function(){
-  $(function(){
 
-    // function buildFileField = (num)=>{
-    //   let html=`<div class="file_group" data-index="${num}><label class="label-box" for="product_images_attributes_${num}_image"><p>クリックしてファイルをアップロード</p><input class="hidden-field" type="file" name="product[images_attributes][${num}][image]" id="product_images_attributes_${num}_image"></laebl></div>`
-    //   return html;
-    // }
+  $(document).on('click', '.image_upload', function(){
 
-    //プレビューのhtmlを定義
-    function buildHTML(count) {
-      let html = `<div class="preview-box" id="preview-box__${count}">
-                    <div class="upper-box">
-                      <img src="" alt="preview">
-                    </div>
-                    <div class="lower-box">
-                      <div class="update-box">
-                        <label class="edit_btn">編集</label>
-                      </div>
-                      <div class="delete-box" id="delete_btn_${count}">
-                        <span>削除</span>
-                      </div>
-                    </div>
-                  </div>`
+    let preview = $('<div class="preview_box"><div class="upper-box"><img class="preview"></div><div class="lower-box"><div class="delete-box">削除</div></div></div>'); 
+  
+    function append_input(num){
+      let  html = $(`<li class="input"><label class="upload-label"><div class="input-area"><input class="hidden image_upload" type="file" name="product[images_attributes][${num}][image]" id = "product_images_attributes_${num}_image"></div></div></label></li>`);
       return html;
     }
 
-    // 投稿編集時
-    //items/:i/editページへリンクした際のアクション=======================================
-    if (window.location.href.match(/\/products\/\d+\/edit/)){
-      //登録済み画像のプレビュー表示欄の要素を取得する
-      var prevContent = $('.label-content').prev();
-      labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
-      $('.label-content').css('width', labelWidth);
-      //プレビューにidを追加
-      $('.preview-box').each(function(index, box){
-        $(box).attr('id', `preview-box__${index}`);
+    $ul = $('#previews')
+    $li = $(this).parents('li');
+    $label = $(this).parents('.upload-label');
+    $inputs = $ul.find('.image_upload');
+
+    $('.image_upload').on('change', function (e) {
+
+      let reader = new FileReader();
+      
+      // プレビューに追加させるために、inputから画像ファイルを読み込む。
+      reader.readAsDataURL(e.target.files[0]);
+
+      //画像ファイルが読み込んだら、処理が実行される。 
+      reader.onload = function(e){
+
+        $(preview).find('.preview').attr('src', e.target.result);
+      }
+
+      $li.append(preview);
+
+      $label.css('display','none'); 
+      $li.removeClass('input');
+      $li.addClass('image-preview'); 
+      $lis = $ul.find('.image-preview'); 
+      $('#previews li').css({
+        'width': `100px`
       })
-      //削除ボタンにidを追加
-      $('.delete-box').each(function(index, box){
-        $(box).attr('id', `delete_btn_${index}`);
+
+
+      if($lis.length <= 4 ){
+        $ul.append(append_input)
+        $('#previews li:last-child').css({
+          'width': `calc(100% - (20% * ${$lis.length}))`
+        })
+      }
+      else if($lis.length == 5 ){
+        $li.addClass('image-preview');
+        $ul.append(append_input)
+        $('#previews li:last-child').remove();
+      }
+
+      $inputs.each( function( num, input ){
+        
+        $(input).removeAttr('name');
+        $(input).attr({
+          name:"product[images_attributes][" + num + "][image]",
+          id:"product_images_attributes_" + num + "_image"
+        });
+      });
+    })
+  })
+
+  $(document).on('click','.delete-box',function(){
+    let append_input = $(`<li class="input"><label class="upload-label"><div class="input-area"><input class="hidden image_upload" type="file" ></div></div></label></li>`)
+    $ul = $('#previews')
+    $lis = $ul.find('.image-preview');
+    $input = $ul.find('.input');
+    $ul = $('#previews')
+    $li = $(this).parents('.image-preview');
+  
+  
+    $li.remove();
+  
+    $lis = $ul.find('.image-preview');
+    $label = $ul.find('.input');
+    if($lis.length <= 4 ){
+      $('#previews li:last-child').css({
+        'width': `calc(100% - (20% * ${$lis.length}))`
       })
-      var count = $('.preview-box').length;
-      //プレビューが5あるときは、投稿ボックスを消しておく
-      if (count == 5) {
-        $('.label-content').hide();
-      }
+      // $ul.append(append_input)
     }
-    // =============================================================================
-
-    // ラベルのwidth操作
-    function setLabel() {
-      //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
-      var prevContent = $('.label-content').prev();
-      labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
-      $('.label-content').css('width', labelWidth);
+    else if($lis.length == 5 ){
+      $('#previews li:last-child').css({
+        'width': `100%`
+      })
     }
 
-    // $('#hidden-content').on("change", '.hidden-field', function(e){
-    //   $('#hidden-content').append
-    // });
-
-    // プレビューの追加
-    $(document).on('change', '.hidden-field', function() {
-      setLabel();
-      //hidden-fieldのidの数値のみ取得
-      var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      //labelボックスのidとforを更新
-      $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-      //選択したfileのオブジェクトを取得
-      var file = this.files[0];
-      var reader = new FileReader();
-      //readAsDataURLで指定したFileオブジェクトを読み込む
-      reader.readAsDataURL(file);
-      //読み込み時に発火するイベント
-      reader.onload = function() {
-        var image = this.result;
-        //プレビューが元々なかった場合はhtmlを追加
-        if ($(`#preview-box__${id}`).length == 0) {
-          var count = $('.preview-box').length;
-          var html = buildHTML(id);
-          //ラベルの直前のプレビュー群にプレビューを追加
-          var prevContent = $('.label-content').prev();
-          $(prevContent).append(html);
-        }
-        //イメージを追加
-        $(`#preview-box__${id} img`).attr('src', `${image}`);
-        var count = $('.preview-box').length;
-        //プレビューが5個あったらラベルを隠す 
-        if (count == 5) { 
-          $('.label-content').hide();
-        }
-
-        //プレビュー削除したフィールドにdestroy用のチェックボックスがあった場合、チェックを外す=============
-        if ($(`#item_images_attributes_${id}__destroy`)){
-          $(`#item_images_attributes_${id}__destroy`).prop('checked',false);
-        } 
-        //=============================================================================
-
-        //ラベルのwidth操作
-        setLabel();
-        //ラベルのidとforの値を変更
-        if(count < 5){
-          $('.label-box').attr({id: `label-box--${count}`,for: `item_images_attributes_${count}_image`});
-        }
-      }
-    });
-
-    // 画像の削除
-    $(document).on('click', '.delete-box', function() {
-      var count = $('.preview-box').length;
-      setLabel(count);
-      var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      $(`#preview-box__${id}`).remove();
-
-      //新規登録時と編集時の場合分け==========================================================
-
-      //新規投稿時
-      //削除用チェックボックスの有無で判定
-      if ($(`#item_images_attributes_${id}__destroy`).length == 0) {
-        //フォームの中身を削除 
-        $(`#item_images_attributes_${id}_image`).val("");
-        var count = $('.preview-box').length;
-        //5個めが消されたらラベルを表示
-        if (count == 4) {
-          $('.label-content').show();
-        }
-        setLabel(count);
-        if(id < 5){
-          $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-
-        }
-      } else {
-
-        //投稿編集時
-        $(`#item_images_attributes_${id}__destroy`).prop('checked',true);
-        //5個めが消されたらラベルを表示
-        if (count == 4) {
-          $('.label-content').show();
-        }
-
-        //ラベルのwidth操作
-        setLabel();
-        //ラベルのidとforの値を変更
-        //削除したプレビューのidによって、ラベルのidを変更する
-        if(id < 5){
-          $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-        }
-      }
-      //=============================================================================
-    });
   });
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_150107) do
+ActiveRecord::Schema.define(version: 2020_09_11_030905) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_150107) do
     t.bigint "user_id"
     t.bigint "category_id", null: false
     t.integer "size_id", null: false
+    t.integer "brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
