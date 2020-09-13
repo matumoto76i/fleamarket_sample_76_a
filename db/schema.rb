ba7540e4dfa2367ef262920edeae1948143ebf21
matumoto76i <34masamasa52@gmail.com>
2020-09-13 14:55:52 +0900

    db順番変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_113113) do
+ActiveRecord::Schema.define(version: 2020_09_09_134335) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id", null: false
