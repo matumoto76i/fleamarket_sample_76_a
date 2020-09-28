196bfcb4777421332779e588dc20661a817b20bb
matumoto76i <34masamasa52@gmail.com>
2020-09-29 00:05:38 +0900

    非同期通信の実装（jbuilder前まで）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/products/#{comment.product.id}"
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to product_path(params[:product_id])}
+      format.json
+    end
   end
 
   private
