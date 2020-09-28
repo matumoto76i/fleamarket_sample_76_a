f547636f448f7fd2a57a5f27458a5946c32e7f6b
matumoto76i <34masamasa52@gmail.com>
2020-09-29 01:57:50 +0900

    jbuilderとjsファイルの編集

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,8 +1,16 @@
 $(function(){
+  function buildHTML(comment){
+    var html = `<p>
+                  <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                  :
+                  ${comment.text}
+                </p>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -10,6 +18,15 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comment_index').append(html);
+      $('.comment_text_box').val('');
+      $('.comment_btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
     })
   })
 })
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
     respond_to do |format|
-      format.html { redirect_to product_path(params[:product_id])}
+      format.html { redirect_to product_path(params[:product_id]) }
       format.json
     end
   end
